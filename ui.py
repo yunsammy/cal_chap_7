16cfbba882a5868f6517c50ff78264b6dc66d486
yunsammy <yun3.jo@gmail.com>
2024-04-20 21:06:59 +0900

    Modify ui.py to add % operator in Qcombobox

--- ui.py
+++ ui.py
@@ -68,5 +68,47 @@ class Control:
         except Exception as e:
             return e
         
+<<<<<<< HEAD
         return a%b
     
+=======
+        self.le1=QLineEdit('0',self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+        self.le1.setFocus(True)
+        self.le1.selectAll()
+        
+        self.le2=QLineEdit('0',self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
+        
+        self.cb = QComboBox(self)
+        self.cb.addItems(['+', '-', '*', '/', '^', '%'])
+        
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+        
+        vbox=QVBoxLayout()
+        vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        
+        self.setLayout(vbox)
+        
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256,256)
+        self.show()
+        
+    def setDisplay(self, text):
+        self.te1.appendPlainText(text)
+        
+    def clearMessage(self):
+        self.te1.clear()
+>>>>>>> 8f2761c (Modify ui.py to add % operator in Qcombobox)
